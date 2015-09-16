d39d56e245283969b10435441d806ae550fcd011
Shawn Jordan <sjordan@hbs.edu>
2015-09-16 16:07:41 -0400

    --all

---
+++ README.md
@@ -0,0 +1,27 @@
+# Shawn's CSCI E-15 Portfolio
+
+# Live URL
+
+[insert url]
+
+## Description
+
+A simple page containing some biographical information about me and linking you to my Dynamic Web Application projects.  The site will change over time as the semester progresses.
+
+## Demo
+
+Normally, this is where I would include a link to demo of the page.
+
+## Details for Teaching Team
+
+There is no login required for this site.  I linked to CSS Skeleton, Normalize and general Bootstrap CDNs with the intention of making the site appear neat and clean, and to make alignment and lists easier to work with.  I added some scripts for Adobe Edge fonts, and I used a tiny bit of PHP to show the current date in the header.  I borrowed the Harvard Extension School logo for the banner and added a picture of myself.  
+
+## Outside Code
+
+Bootstrap CSS:  http://getbootstrap.com/
+
+Skeleton CSS:  http://getskeleton.com/
+
+Normalize CSS:  https://necolas.github.io/normalize.css/
+
+Edge Fonts:  https://edgewebfonts.adobe.com/
---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>Shawn's CSCI E-15 Portfolio</title>
+	<link rel="stylesheet" href="src/css/p1.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" />
+	<script src="http://use.edgefonts.net/architects-daughter:n4:all;josefin-sans:i7,n7,n3,i4,i3,n4,i1,n6,n1,i6:all.js"></script>
+	<meta charset="utf-8">
+</head>
+
+<body>
+	<div class="container">
+		<header>
+			<ul class="row">
+				<li class="four columns"><img src="src/img/ext_sch_logo.png" alt="Extension School Logo"></li>
+				<ul class="four columns">
+					<li>
+						<h1>DWA15 Portfolio</h1></li>
+					<li>
+						<h2>Shawn Jordan</h2></li>
+					<li>
+						<p class="date">
+							<?php echo date("m.d.Y"); ?>
+						</p>
+					</li>
+				</ul>
+				<li class="three columns"><img src="src/img/prof_pic2.jpg" alt="Me"></li>
+			</ul>
+		</header>
+		<nav>
+			<ul class="row">
+				<li class="four columns">Project 2 Title | View:[link] Github:[link]</li>
+				<li class="four columns">Project 3 Title | View:[link] Github:[link]</li>
+				<li class="four columns">Project 4 Title | View:[link] Github:[link]</li>
+			</ul>
+		</nav>
+		<div class="main">
+			<h4>In 2+ sentences describe who you are and what you do.</h4>
+			<p>I'm Shawn, and I am a fan of American black metal, an avid reader of Steven Erikson's <em>Malazan Book of the Fallen</em>, and an avant garde cook specializing in random mixes of what's left in the fridge. I work at Harvard Business School and I have a master's degree in philosophy.</p>
+			<h4>Why are you taking this course?</h4>
+			<p>I'm interested in a career path in technical writing. 'Technical writer' isn't an unambiguous job title, but in it's various forms and in different industries it often involves some degree of programming and/or web development skill. So, in taking this class I am hoping to improve my chances of finding work along this career path.</p>
+			<h4>What previous experience do you have with programming?</h4>
+			<p>Very little. I took CSCI E-12 in the summer so I feel fairly comfortable with a very small amount of programming in Java Script and PHP. But, from what I understand (and from what my experience thus far tells me) learning a programming language is not unlike learning formal logic, and I have quite a bit of training in formal logic. So, sometimes a bit of programming that I'm learning for the first time looks familiar to me, which can be an advantage.</p>
+			<h4>Are you working on a Mac, PC, or some other platform?</h4>
+			<p>I am working on a Dell PC running Windows 10.</p>
+			<h4>Anything else we should know?</h4>
+			<p>When it's left up to me to arbitrarily name a project or site, I tend to pick terms from philosophy (specifically, metaphysics). This is why, for example, my class-devoted URL includes the term 'supervene'.</p>
+		</div>
+	</div>
+</body>
+
+</html>
